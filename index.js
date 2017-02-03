9a0fdc737099d6cad1d5b62b5669ccbda004cac5
Stephanie Cure <e.cure.ch@gmail.com>
2017-02-03 18:15:39 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+class Character {
+  constructor (attackPower, health){
+    this.attackPower = attackPower;
+    this.health = health;
+  }
+  //methods
+  attack() {
+    // function to attack oponent if Math.random() > 0.5
+  }
+
+
+}
+
+
+class Blue extends Character{
+
+}
+
+
+class Red extends Character {
+
+}
