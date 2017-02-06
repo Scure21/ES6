2b5b34130c7463d4c62ab3f1bea8eab04a5f0a8a
Stephanie Cure <e.cure.ch@gmail.com>
2017-02-06 11:03:57 -0500

    got till let

--- index.js
+++ index.js
@@ -4,19 +4,35 @@ class Character {
     this.health = health;
   }
   //methods
-  attack() {
+  attack(opponent) {
     // function to attack oponent if Math.random() > 0.5
+    if (Math.random > 0.5) opponent.health =- 1
   }
-
-
 }
 
 
 class Blue extends Character{
-
+  constructor(attackPower, health){
+    super(attackPower, health);
+  }
+  //methods
 }
 
 
 class Red extends Character {
-
+  constructor(attackPower, health){
+    super(attackPower, health);
+  }
+  //methods
 }
+
+
+//rewriting map with arrow function
+var winnerArr = ['red', 'red', 'blue', 'blue', 'red'];
+
+winnerArr.map((item, index)=>console.log({winner: item}));
+
+//let and const
+let foo = 'bar';
+console.log(foo);
+
